537437b883e073d8f954d0055727f2f83f9c55c1
alexa <alexadeleted@gmail.com>
2022-08-31 16:14:03 +0300

    final

--- src/main/java/dev/alexa/sensor/domain/Sensor.java
+++ src/main/java/dev/alexa/sensor/domain/Sensor.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Table
 public class Sensor {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String model;
